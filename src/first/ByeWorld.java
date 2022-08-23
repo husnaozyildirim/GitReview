5d3e19a0c1aa3f7a4d6d74a51a617daaf5cfe9ea
Husna <ozyildirimhusna@gmail.com>
2022-08-23 11:31:41 -0400

    ByeWorld

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("haft way of java, doing good");
+        System.out.println("learning Git class");
+    }
+}
